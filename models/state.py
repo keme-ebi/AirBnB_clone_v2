3e14f5f63296f2f023c6018bae20d4ba1c1e2774
keme-ebi <kelvin6bolou@gmail.com>
2024-01-02 16:54:28 +0000

    update

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 import models
 from sqlalchemy.orm import relationship
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
@@ -13,11 +14,12 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete', backref='states')
 
-    @property
-    def cities(self):
-        """returns the list of city instances"""
-        c_inst = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                c_inst.append(city)
-        return c_inst
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """returns the list of city instances"""
+            c_inst = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    c_inst.append(city)
+            return c_inst
